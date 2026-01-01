7a3e55a4d5eb51dc745066e6e255f996065f5f6a
Mustapha <mustaphabenfateh@gmail.com>
2026-01-01 15:26:35 +0100

    Passer des données dans l’action: (App.js)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useDispatch,useSelector } from 'react-redux';
 function App() {
   const nbre=useSelector(data=>data.nbre)
+  const [inc,setInc]=useState("");
   const dispatch=useDispatch();
   return (
   <div className="App" >
+    <p>Increent:<input type="text" onChange={(e)=>setInc(e.target.value)} /></p>
     <p>Nombre: {nbre}</p>
-    <p><button onClick={()=>dispatch({type:"Incrementer"})}>Incrementer</button></p>
-    <p><button onClick={()=>dispatch({type:"Decrementer"})}>Decrementer</button></p>
+    <p><button onClick={()=>dispatch({type:"Incrementer",
+      payload:inc})}>Incrementer</button></p>
+    <p><button onClick={()=>dispatch({type:"Decrementer", 
+      playload:inc})}>Decrementer</button></p>
     <p><button onClick={()=>dispatch({type:"Reinitialiser"})}>Reinitialiser</button></p>
   </div>
   );
